9369f7bbfaf8b613d51dc2cff1f86f2368307f04
IfJosean <josean7jkami@gmail.com>
2017-09-21 12:26:53 +0200

    +++ figura

--- src/main/Figura.java
+++ src/main/Figura.java
@@ -3,9 +3,11 @@ package main;
 /**
  * Created by Josean on 18/09/2017.
  */
-public abstract class Figura implements Comparable{
+public abstract class Figura implements Comparable<Figura{
 
-    double area;
+    public abstract double area();
+
+
 
 
 }
